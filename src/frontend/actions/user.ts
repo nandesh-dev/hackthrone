cfe8623e5f4b8fc329cbe385f971573f040eb4e1
nandesh-dev <nandesh.dev@gmail.com>
2025-10-29 19:00:47 +0000

    feat: add home page

--- src/frontend/actions/user.ts
+++ src/frontend/actions/user.ts
@@ -9,3 +9,47 @@ export async function getUser() {
     };
   }
 }
+
+export async function getUserStatistics() {
+  if (import.meta.env.VITE_MOCK_MODE) {
+    await delay(300);
+    return {
+      creditsEarned: 541,
+      projects: 4,
+      collaborators: 16,
+      history: [
+        { date: "2025-09-30", words: 2, credits: 2 },
+        { date: "2025-10-01", words: 1, credits: 2 },
+        { date: "2025-10-02", words: 2, credits: 1 },
+        { date: "2025-10-03", words: 2, credits: 3 },
+        { date: "2025-10-04", words: 4, credits: 3 },
+        { date: "2025-10-05", words: 3, credits: 3 },
+        { date: "2025-10-06", words: 2, credits: 2 },
+        { date: "2025-10-07", words: 4, credits: 3 },
+        { date: "2025-10-08", words: 1, credits: 1 },
+        { date: "2025-10-09", words: 3, credits: 2 },
+        { date: "2025-10-10", words: 3, credits: 4 },
+        { date: "2025-10-11", words: 3, credits: 2 },
+        { date: "2025-10-12", words: 3, credits: 4 },
+        { date: "2025-10-13", words: 1, credits: 2 },
+        { date: "2025-10-14", words: 1, credits: 2 },
+        { date: "2025-10-15", words: 1, credits: 2 },
+        { date: "2025-10-16", words: 4, credits: 4 },
+        { date: "2025-10-17", words: 4, credits: 4 },
+        { date: "2025-10-18", words: 2, credits: 2 },
+        { date: "2025-10-19", words: 1, credits: 2 },
+        { date: "2025-10-20", words: 1, credits: 2 },
+        { date: "2025-10-21", words: 2, credits: 2 },
+        { date: "2025-10-22", words: 1, credits: 2 },
+        { date: "2025-10-23", words: 4, credits: 3 },
+        { date: "2025-10-24", words: 2, credits: 3 },
+        { date: "2025-10-25", words: 1, credits: 1 },
+        { date: "2025-10-26", words: 4, credits: 4 },
+        { date: "2025-10-27", words: 1, credits: 2 },
+        { date: "2025-10-28", words: 3, credits: 2 },
+        { date: "2025-10-29", words: 5, credits: 4 },
+        { date: "2025-10-30", words: 2, credits: 2 },
+      ],
+    };
+  }
+}
--- src/frontend/pages/home.tsx
+++ src/frontend/pages/home.tsx
@@ -1,5 +1,247 @@
 import { Layout } from "@/components/layout";
+import { IconTrendingDown, IconTrendingUp } from "@tabler/icons-react";
+import * as React from "react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import { useQuery } from "@tanstack/react-query";
+import { getUserStatistics } from "@/actions/user";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export function HomePage() {
-  return <Layout>Hi</Layout>;
+  const userStatisticsQuery = useQuery({
+    queryFn: getUserStatistics,
+    queryKey: ["getUserStatistics"],
+  });
+
+  return (
+    <Layout>
+      <div className="flex flex-col gap-8 px-8">
+        <SectionCards data={userStatisticsQuery.data} />
+        {userStatisticsQuery.isSuccess && (
+          <ChartAreaInteractive data={userStatisticsQuery.data?.history} />
+        )}
+      </div>
+    </Layout>
+  );
+}
+
+export function SectionCards({ data }: { data: any }) {
+  return (
+    <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
+      <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Credits</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            {data?.creditsEarned || "- -"}
+          </CardTitle>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">
+            Trending up this month <IconTrendingUp className="size-4" />
+          </div>
+          <div className="text-muted-foreground">
+            Total number of credits earned
+          </div>
+        </CardFooter>
+      </Card>
+      <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Projects</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            {data?.projects || "- -"}
+          </CardTitle>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">
+            Decreasing slightly this year{" "}
+            <IconTrendingDown className="size-4" />
+          </div>
+          <div className="text-muted-foreground">
+            Total number of projects you worked on
+          </div>
+        </CardFooter>
+      </Card>
+      <Card className="@container/card">
+        <CardHeader>
+          <CardDescription>Collaborators</CardDescription>
+          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+            {data?.collaborators}
+          </CardTitle>
+        </CardHeader>
+        <CardFooter className="flex-col items-start gap-1.5 text-sm">
+          <div className="line-clamp-1 flex gap-2 font-medium">
+            Strong collaborations <IconTrendingUp className="size-4" />
+          </div>
+          <div className="text-muted-foreground">
+            Number of collaborators on your projects
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
+export const description = "An interactive area chart";
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  words: {
+    label: "Words",
+    color: "var(--chart-1)",
+  },
+  credits: {
+    label: "Credits",
+    color: "var(--chart-2)",
+  },
+} satisfies ChartConfig;
+
+export function ChartAreaInteractive({ data }: { data: any[] }) {
+  const [timeRange, setTimeRange] = React.useState("90d");
+
+  const filteredData = data?.filter((item: any) => {
+    const date = new Date(item.date);
+    let daysToSubtract = 90;
+    if (timeRange === "30d") {
+      daysToSubtract = 30;
+    } else if (timeRange === "7d") {
+      daysToSubtract = 7;
+    }
+    const startDate = new Date("2025-10-30");
+    startDate.setDate(startDate.getDate() - daysToSubtract);
+    return date >= startDate;
+  });
+
+  return (
+    <Card className="pt-0">
+      <CardHeader className="flex items-center gap-2 space-y-0 border-b py-5 sm:flex-row">
+        <div className="grid flex-1 gap-1">
+          <CardTitle>Credit History</CardTitle>
+          <CardDescription>
+            Showing the number of credits earned
+          </CardDescription>
+        </div>
+        <Select value={timeRange} onValueChange={setTimeRange}>
+          <SelectTrigger
+            className="hidden w-[160px] rounded-lg sm:ml-auto sm:flex"
+            aria-label="Select a value"
+          >
+            <SelectValue placeholder="Last 3 months" />
+          </SelectTrigger>
+          <SelectContent className="rounded-xl">
+            <SelectItem value="90d" className="rounded-lg">
+              Last 3 months
+            </SelectItem>
+            <SelectItem value="30d" className="rounded-lg">
+              Last 30 days
+            </SelectItem>
+            <SelectItem value="7d" className="rounded-lg">
+              Last 7 days
+            </SelectItem>
+          </SelectContent>
+        </Select>
+      </CardHeader>
+      <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
+        <ChartContainer
+          config={chartConfig}
+          className="aspect-auto h-[250px] w-full"
+        >
+          <AreaChart data={filteredData}>
+            <defs>
+              <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--chart-1)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--chart-1)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+              <linearGradient id="fillMobile" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor="var(--chart-2)"
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor="var(--chart-2)"
+                  stopOpacity={0.1}
+                />
+              </linearGradient>
+            </defs>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              minTickGap={32}
+              tickFormatter={(value) => {
+                const date = new Date(value);
+                return date.toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                });
+              }}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent
+                  labelFormatter={(value) => {
+                    return new Date(value).toLocaleDateString("en-US", {
+                      month: "short",
+                      day: "numeric",
+                    });
+                  }}
+                  indicator="dot"
+                />
+              }
+            />
+            <Area
+              dataKey="credits"
+              type="natural"
+              fill="url(#fillMobile)"
+              stroke="var(--color-1)"
+              stackId="a"
+            />
+            <Area
+              dataKey="words"
+              type="natural"
+              fill="url(#fillDesktop)"
+              stroke="var(--color-2)"
+              stackId="a"
+            />
+            <ChartLegend content={<ChartLegendContent />} />
+          </AreaChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
 }
