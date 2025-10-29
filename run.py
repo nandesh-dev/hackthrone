f01d9c7d681bbb20723f942a82da3355ff8d5fc7
SockYeh <sahranashfaq@gmail.com>
2025-10-29 11:08:05 +0530

    feat: add host ip and port to env

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
 import uvicorn
+from src.backend.config import env
 
 if __name__ == "__main__":
-    uvicorn.run("src.backend.main:app", reload=True)
+    uvicorn.run(
+        "src.backend.main:app",
+        host=env.HOST_IP,
+        port=env.HOST_PORT,
+        reload=True,
+    )
--- src/backend/config.py
+++ src/backend/config.py
@@ -1,10 +1,14 @@
 """Module for configuration settings."""
 
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     """Settings for the application."""
+
+    HOST_IP: str = Field()
+    HOST_PORT: int = Field()
 
     model_config = SettingsConfigDict(env_file=".env")
 
